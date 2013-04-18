d4fd70e2e6177f8efeb60ce6ce001f66e2f1edff
usama <usama@usama-VGN-CR353-B.(none)>
2013-04-18 20:42:13 +0500

    Active Admin Added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 ShoppingGuide::Application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  
+  root :to => "home#index"
+
+  ActiveAdmin.routes(self)
+
   get "home/index"
 
 
@@ -68,7 +74,6 @@ ShoppingGuide::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => "home#index"
 
   # See how all your routes lay out with "rake routes"
 
