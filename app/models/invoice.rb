15e87dbf0e93d3e1049c916c6867e7cac347d648
Haseeb Ahmad <engg.haseeb@gmail.com>
2013-04-22 17:02:37 +0500

    Active Admin InvoiceProduct ActiveAdmin Bug

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -2,4 +2,7 @@ class Invoice < ActiveRecord::Base
   attr_accessible :invoice_number
   has_many :invoice_products
   has_many :products, :through => :invoice_products
+
+  alias_attribute :name, :invoice_number
+
 end
